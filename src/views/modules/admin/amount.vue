6b064c7cdaf93899dd119fb09ffd63583808cd89
馒头煤球 <13737914+mantou-briquette@user.noreply.gitee.com>
2023-12-05 21:01:40 +0800

    pages

--- src/views/modules/admin/amount.vue
+++ src/views/modules/admin/amount.vue
@@ -1,16 +1,23 @@
 <template>
-	<div>
-		<el-form-item label="累计交易"><el-input></el-input></el-form-item>
+	<div style="width:300px">
+        <el-form label-width="80px">
+
+            <el-form-item label="累计交易"><el-input></el-input></el-form-item>
 		<el-form-item label="累计客户"><el-input></el-input></el-form-item>
+    
+  <el-form-item>
+    <el-button type="primary" @click="onSubmit">保存</el-button>
+  </el-form-item>
+        </el-form>
 	</div>
 </template>
 
 <script>
 export default {
-	data() {
-		return {
-			info: {},
-		};
-	},
-};
+  data () {
+    return {
+      info: {}
+    }
+  }
+}
 </script>
