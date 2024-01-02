7dd89844e79135bfc97815bc09a55b7d4bf9d20a
馒头煤球 <13737914+mantou-briquette@user.noreply.gitee.com>
2024-01-02 20:40:44 +0800

    fix

--- src/components/file-upload/index.vue
+++ src/components/file-upload/index.vue
@@ -7,6 +7,7 @@
 			:show-file-list="false"
 			:on-success="handleUploadSuccess"
 			:on-progress="onUploadProgress"
+            :on-error="onUploadError"
 			:before-upload="beforeAvatarUpload"
 		>
 			<el-progress
@@ -14,7 +15,7 @@
 				v-if="progress.loaded"
 				:percentage="Math.floor((progress.loaded / progress.total) * 100)"
 			></el-progress>
-			<video v-if="value" :src="value"></video>
+			<video style="width:90%;margin-top:6px" v-if="value" :src="value"></video>
 			<i
 				v-if="!value && !progress.loaded"
 				class="el-icon-plus pic-uploader-icon"
@@ -26,25 +27,28 @@
 				@click.stop="handeRemove"
 				>删除</el-button
 			>
+
+            <p v-if="err" class="filerr">1111{{ err }}</p>
 		</el-upload>
 	</div>
 </template>
 
 <script>
 export default {
-	data() {
-		return {
-			resourcesUrl: process.env.VUE_APP_RESOURCES_URL,
-			progress: { loaded: 0, total: 100 },
-		};
-	},
-	props: {
-		value: {
-			default: "",
-			type: String,
-		},
-	},
-	computed: {
+  data () {
+    return {
+      resourcesUrl: process.env.VUE_APP_RESOURCES_URL,
+      progress: { loaded: 0, total: 100 },
+      err: ''
+    }
+  },
+  props: {
+    value: {
+      default: '',
+      type: String
+    }
+  },
+  computed: {
 		//   fileList () {
 		//     let res = []
 		//     if (this.value) {
@@ -56,39 +60,45 @@ export default {
 		//     this.$emit('input', this.value)
 		//     return res
 		//   }
-	},
-	methods: {
+  },
+  methods: {
 		// 图片上传
-		handleUploadSuccess(response, file, fileList) {
-			this.$emit("change", this.resourcesUrl + "images/" + file.response.data);
-			this.$emit("input", this.resourcesUrl + "images/" + file.response.data);
-			this.progress = { loaded: 0, total: 100 };
-		},
+    handleUploadSuccess (response, file, fileList) {
+      this.$emit('change', this.resourcesUrl + 'images/' + file.response.data)
+      this.$emit('input', this.resourcesUrl + 'images/' + file.response.data)
+      this.progress = { loaded: 0, total: 100 }
+      this.err = ''
+    },
 		// 限制图片上传大小
-		beforeAvatarUpload(file) {
-			const isVideo = file.type === "video/mp4";
-			if (!isVideo) {
-				this.$message.error("上传视频只能是mp4 格式!");
-			}
-			const isLt2M = file.size / 1024 / 1024 < 400;
-			if (!isLt2M) {
-				this.$message.error("上传视频大小不能超过 400MB!");
-			}
-			return isLt2M;
-		},
-		onUploadProgress(e, file, fileList) {
-			console.log(e, file, fileList);
-			this.progress = e;
-		},
-		handeRemove() {
-			this.$emit("change", "");
-			this.$emit("input", "");
-		},
-		beforeRemove(file, fileList) {
-			return this.$confirm(`确定移除 ${file.name}？`);
-		},
-	},
-};
+    beforeAvatarUpload (file) {
+      const isVideo = file.type === 'video/mp4'
+      if (!isVideo) {
+        this.$message.error('上传视频只能是mp4 格式!')
+      }
+      const isLt2M = file.size / 1024 / 1024 < 400
+      if (!isLt2M) {
+        this.$message.error('上传视频大小不能超过 400MB!')
+      }
+      return isLt2M
+    },
+    onUploadProgress (e, file, fileList) {
+      console.log(e, file, fileList)
+      this.err = ''
+      this.progress = e
+    },
+    onUploadError () {
+      this.err = '上传失败，请检查'
+      this.progress = { loaded: 0, total: 100 }
+    },
+    handeRemove () {
+      this.$emit('change', '')
+      this.$emit('input', '')
+    },
+    beforeRemove (file, fileList) {
+      return this.$confirm(`确定移除 ${file.name}？`)
+    }
+  }
+}
 </script>
 
 <style lang="scss">
@@ -108,7 +118,10 @@ export default {
 .del-file {
 	color: #e34444;
 	position: absolute;
-	right: -60px;
+	right: -10px;
 	top: 0;
+}
+.filerr{
+	color: #e34444;
 }
 </style>
