fafa2add8fed327708308002b5c4b4344faa5d09
tong <904726194@qq.com>
2025-05-28 13:30:32 +0800

    fix

--- src/crud/user/user.js
+++ src/crud/user/user.js
@@ -27,18 +27,19 @@ export const tableOption = {
     {
       label: '注册审核',
       prop: 'userMemo',
+      search: true,
       dicData: [
         {
           label: '未审核',
           value: 0
         },
         {
-          label:'未通过',
-          value:'2'
+          label: '未通过',
+          value: '2'
         },
         {
-          label:'已通过',
-          value:'1'
+          label: '已通过',
+          value: '1'
         }
       ],
       formatter(row){
